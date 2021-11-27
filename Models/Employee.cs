9ef86cd15ed1e8e384d2fa0ff48e36dea17fdc35
Dreaith <noel80@hotmail.ca>
2021-11-26 23:55:49 -0500

    second

---
+++ Models/Employee.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Rocket_Elevators_REST_API.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string function { get; set; }
+        public string email { get; set; }
+        public int user_id { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+     
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
