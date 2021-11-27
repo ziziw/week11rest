ee424dc33c51e4a0b0ab0a3d8cac7421891ea534
longviddd <73258079+longviddd@users.noreply.github.com>
2021-11-26 23:19:31 -0500

    Created employee.cs

---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+using System; 
+namespace Rocket_Elevators_REST_API.Models
+{
+  public class Employee
+  {
+    public int id { get; set; }
+    public string first_name { get; set; }
+    public string last_name {get; set;}
+    public string function {get; set;}
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+    public int user_id { get; set; }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return "Employee ID: " + this.id;
+        }
+  }
+}
